f2ea5eab4e0a438dee76378533850d5797eac5c3
Sanderson Rocha <sanduba753@gmail.com>
2026-02-26 12:55:50 -0300

    Lint adequation

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { entityList } from '@src/configs/entity';
 import { migrationList } from '@src/configs/migration';
-import { join } from 'node:path';
+// import { join } from 'node:path';
 
 export const dataSource: TypeOrmModuleOptions = {
   type: 'sqlite',
